7bf1f1438d8d6c43eefdb1080a691daf37ec551b
Francesco Lombardi <francesco.lombardi@polimi.it>
2019-11-22 09:53:53 +0100

    Add files via upload

--- test_operation/nos_model_run.py
+++ test_operation/nos_model_run.py
@@ -10,13 +10,14 @@ A script to run and post-process the Italy Calliope 20-node model based on a NOS
 import calliope
 import numpy as np
 import calliope.core.io
-
+from  dispatch_plots import power_plot
 
 calliope.set_log_verbosity('INFO') #sets the level of verbosity of Calliope's operations
 
 #%% 
 
 model = calliope.Model('Model/model.yaml')
-model.run(build_only=True)
+model.run()
 
+#model.plot.summary(to_file='summary.html')
 
